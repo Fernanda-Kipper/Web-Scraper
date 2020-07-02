5e02cc6c3991602f3c00fdfcc10368a68d8caa87
Fernanda Kipper Bucoski de Sousa <61896274+Fernanda-Kipper@users.noreply.github.com>
2020-07-02 18:34:09 -0300

    Update ploting_data.py

--- ploting_data.py
+++ ploting_data.py
@@ -32,6 +32,9 @@ def ploting_results():
     plt.xlabel("Data")
     plt.annotate(f"Média: R${mean:.2f} \nDesvio Padrão: R${std_dv:.2f}", xy=(0, 1.5), size=8,
                  bbox=dict(boxstyle="round", alpha=0.2))
+    
+    plt.yticks(rotation=10)
+    plt.xticks(rotation=10)
 
     plt.style.use("dark_background")
     plt.plot_date(datas, prices_kayak, linestyle="solid", linewidth=2, color="#ae7ac7", label="kayak")
